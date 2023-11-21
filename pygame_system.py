fd3d9955dcb25f68a87de0cdad73783a70578d40
c0b2206374 <c0b2206374@edu.teu.ac.jp>
2023-11-21 14:37:23 +0900

    コミット

--- pygame_system.py
+++ pygame_system.py
@@ -8,7 +8,7 @@ def main():
     font = pg.font.Font(None, 80)
 
     enn = pg.Surface((20, 20))
-    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
+    pg.draw.circle(enn, (255, 255, 0), (10, 10), 10)
     enn.set_colorkey((0, 0, 0))
 
     tmr = 0
@@ -21,8 +21,8 @@ def main():
         screen.blit(txt, [300, 200])
         screen.blit(enn, [100, 400])
         pg.display.update()
-        tmr += 1        
-        clock.tick(1)
+        tmr += 1
+        clock.tick(100)
 
 
 if __name__ == "__main__":
