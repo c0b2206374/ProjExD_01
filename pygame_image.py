e1258b5baf27ed15b4b43ff6d2020aefa3460a0a
c0b2206374 <c0b2206374@edu.teu.ac.jp>
2023-11-21 15:10:42 +0900

    コミット1

---
+++ pygame_image.py
@@ -0,0 +1,27 @@
+import sys
+import pygame as pg
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((1600, 900))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("ex01-20231121/fig/pg_bg.jpg")
+    kou_img = pg.image.load("ex01-20231121/fig/3.png")
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])
+        screen.blit(kou_img,[300,300])
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
